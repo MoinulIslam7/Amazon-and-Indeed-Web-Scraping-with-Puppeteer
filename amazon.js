72c0f313f04778fa41f5462243c9e8b8d22cfcca
Moinul78 <moinul@coredvs.ltd>
2024-01-09 12:28:00 +0600

    update on data collecting

--- amazon.js
+++ amazon.js
@@ -23,35 +23,32 @@ const puppeteer = require('puppeteer');
 
     // for each element attempt to retrieve the title if there is one (some elements in here may not have a title)
     try {
-      title = await page.evaluate(el => el.querySelector('div > h2 > a > span').textContent, laptop);
-      console.log(title);
+      title = await page.evaluate(el => el.querySelector('div > h2 > a > span')?.textContent, laptop);
     } catch (err) {
       console.log('no title found for this element');   
       console.log(err);
     }
 
     // for each element try to retrieve the price
-    // try {
-    //   priceDollar = await page.evaluate(el => el.querySelector('span.a-price-whole').textContent, laptop);
-    //   priceCent = await page.evaluate(el => el.querySelector('span.a-price-fraction').textContent, laptop);
-    // } catch(err) {
-    //   console.log('no price found for this element');
-    // }
+    try { 
+      priceDollar = await page.evaluate(el => el.querySelector('span .a-offscreen')?.textContent, laptop);
+    } catch(err) {
+      console.log('no price found for this element');
+    }
 
     // for each element try to retrieve the image url
-    // try {
-    //   image = await page.evaluate(el => el.querySelector('.s-image').getAttribute('src'), laptop);
-    // } catch(err) {
-    //   console.log('no image found for element');
-    // }
+    try {
+      image = await page.evaluate(el => el.querySelector('.s-image').getAttribute('src'), laptop);
+    } catch(err) {
+      console.log('no image found for element');
+    }
     
     // for each element try to receive page URL
-    // try {
-    //   url = await page.evaluate(el => el.querySelector('.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal').href, laptop);
-    // } catch (err) {
-    //   console.log('No URL found for this element');
-    // }
-    
+    try {
+      url = await page.evaluate(el => el.querySelector('.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal').href, laptop);
+    } catch (err) {
+      console.log('No URL found for this element');
+    }
     if (title !== null) {
       transformedLaptops.push({
         title: title,
